12d321b6539d4e0eef1c4bde82955b9c7ec847f2
Ahmed Shahin <ahmedshahinx0x@gmail.com>
2025-09-23 06:11:36 +0300

    mt6768-common: Initial support for penangf

--- twrp_penangf.mk
+++ twrp_penangf.mk
@@ -14,6 +14,19 @@
 # limitations under the License.
 #
 
+# Inherit from those products. Most specific first.
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-PRODUCT_MAKEFILES += \
-    $(LOCAL_DIR)/twrp_penangf.mk
+# Inherit some common twrp stuff.
+$(call inherit-product, vendor/twrp/config/common.mk)
+
+# Inherit from penangf device
+$(call inherit-product, device/motorola/mt6768/device.mk)
+
+PRODUCT_DEVICE := mt6768
+PRODUCT_NAME := twrp_penangf
+PRODUCT_BRAND := motorola
+PRODUCT_MODEL := mt6768
+PRODUCT_MANUFACTURER := motorola
+
